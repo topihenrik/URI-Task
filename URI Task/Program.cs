29609dccb1f6b6d1a4a5e50e2259b64393d49abb
Topi Ilmoni <topi.ilmoni@gmail.com>
2023-01-30 18:47:17 +0200

    Add project files.

---
+++ URI Task/Program.cs
@@ -0,0 +1,53 @@
+﻿using UriValidation;
+
+namespace URI_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Valid Uniform Resource Identifiers
+            Console.WriteLine("***** Valid URIs *****");
+            testValidator("visma-identity://login?source=severa");
+            testValidator("visma-identity://confirm?source=netvisor&paymentnumber=102226");
+            testValidator("visma-identity://sign?source=vismasign&documentid=105ab44");
+
+            // Invalid Uniform Resource Identifiers
+            Console.WriteLine("***** Invalid URIs *****");
+            testValidator("HelloWorld!");
+            testValidator("https://www.google.com/");
+            testValidator("visx-idtity://login?source=severa");
+            testValidator("visma-identity://grass?source=severa");
+            testValidator("visma-identity://login");
+            testValidator("visma-identity://confirm?source=netvisor&paymentnumber=1xx26");
+            testValidator("visma-identity://sign?source=vismasign&documentid=");
+        }
+
+        static void testValidator(String uri)
+        {
+            Console.WriteLine("Uri to be valitated: " + uri);
+            UriValidator validator = new UriValidator();
+            if (validator.Validate(uri))
+            {
+                Console.WriteLine("Uri is valid.");
+                Console.WriteLine("Path: " + validator.GetPath());
+                Console.WriteLine("Parameters:");
+                if (validator.GetParameters().Count > 0)
+                {
+                    foreach (KeyValuePair<String, String> kvp in validator.GetParameters())
+                    {
+                        Console.WriteLine("\t" + kvp.Key + ": " + kvp.Value);
+                    }
+                } else
+                {
+                    Console.WriteLine("None");
+                }
+                
+            } else
+            {
+                Console.WriteLine("Uri is invalid.");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ URI Task/URIValidator.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Specialized;
+
+namespace UriValidation
+{
+    public class UriValidator
+    {
+        readonly private String validScheme = "visma-identity";
+        readonly private String[] validHosts = { "login", "confirm", "sign" };
+
+        private String path = "";
+        private Dictionary<String, String> parameters = new Dictionary<String, String>();
+
+        // If uri is valid returns true
+        public bool Validate(String unknown_string)
+        {
+            try
+            {
+                // Clear parameters and path for new validation
+                parameters.Clear(); 
+                path = "";
+
+                Uri uri = new Uri(unknown_string);
+
+                if (uri.Scheme != validScheme) // Schema is not valid
+                {
+                    Console.WriteLine("Invalid scheme");
+                    return false;
+                }
+
+                if (!validHosts.Contains(uri.Host)) // Path is not valid
+                {
+                    Console.WriteLine("Invalid path");
+                    return false;
+                } else
+                {
+                    path = uri.Host;
+                }
+
+                
+                NameValueCollection queryParams = System.Web.HttpUtility.ParseQueryString(uri.Query);
+
+                // Check validity of parameters based on path
+                if (uri.Host == "login" && queryParams.Count == 1)
+                {
+                    if (queryParams.AllKeys[0] == "source" && queryParams[0] != null && queryParams[0] != "")
+                    {
+                        Console.WriteLine("Invalid parameters");
+                        return false;
+                    }
+                }
+                else if (uri.Host == "confirm" && queryParams.Count == 2)
+                {
+                    if (queryParams.AllKeys[0] == "source" && queryParams[0] != null && queryParams[0] != "")
+                    {
+                        parameters["source"] = queryParams[0];
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid parameters");
+                        return false;
+                    }
+
+                    if (queryParams.AllKeys[1] == "paymentnumber" && int.TryParse(queryParams[1], out _))
+                    {
+                        parameters["paymentnumber"] = queryParams[1];
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid parameters");
+                        return false;
+                    }
+                }
+                else if (uri.Host == "sign" && queryParams.Count == 2)
+                {
+                    if (queryParams.AllKeys[0] == "source" && queryParams[0] != null && queryParams[0] != "")
+                    {
+                        parameters["source"] = queryParams[0];
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid parameters");
+                        return false;
+                    }
+
+                    if (queryParams.AllKeys[1] == "documentid" && queryParams[1] != null && queryParams[1] != "")
+                    {
+                        parameters["documentid"] = queryParams[1];
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid parameters");
+                        return false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid parameters");
+                    return false;
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return false;
+            }
+        }
+
+        public String GetPath()
+        {
+            return path;
+        }
+
+        public Dictionary<String, String> GetParameters()
+        {
+            return parameters;
+        }
+    }
+}
